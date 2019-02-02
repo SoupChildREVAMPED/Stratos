9070a766a3459e2ca1c6b64270ef12217cfaff0f
SoupChildREVAMPED <47062106+SoupChildREVAMPED@users.noreply.github.com>
2019-02-02 17:48:46 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,6 +18,11 @@ async def dice(number1 = 1, number2 = 6):
 async def stratos(ctx):
     await client.say(f"{ctx.message.author.mention} Geia, sas. What you're looking for can be found here, https://www.roblox.com/groups/4545549/Stratos-tou-Athinon \n \nThis is the main holder of all official infantries of Athenai, if you need any help finding the armies, they'll be found under the allies tab., if you need any assistance about an infantry and their speciality or how they operate, please contact the Genikos' of that certain infantry or any officers from it. \n \nhttps://docs.google.com/document/d/14jgeTUP6Qa2eI9Au76jwaHh-YJkqQyWeHI-TiWD8XbY/edit \n \n That document will provide you with more additional facts/requirements and descriptions about our infantries. \n \nThank you for having the patience to read this!")
 
+@client.event()
+async def on_message(ctx):
+    if ctx.message == "@Stratos":
+        await client.say(f"Hi there {ctx.message.author.mention}. My commands are `-dice` and `-stratos`.")
+
 @client.event
 async def on_ready():
     print("The bot is ready!")
